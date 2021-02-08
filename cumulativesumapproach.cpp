c9578f911cdf43d0e4f01cddb5609d41bc2a2519
subhashish-18 <69270038+subhashish-18@users.noreply.github.com>
2021-02-09 00:32:03 +0530

    Add files via upload

---
+++ cumulativesumapproach.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+//prefix sum technique
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int currsum[n + 1];
+    currsum[0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        currsum[i] = currsum[i - 1] + arr[i - 1];
+    }
+
+    int maxsum = INT_MIN;
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < i - 1; j++)
+        {
+            sum = currsum[i] - currsum[j];
+            maxsum = max(sum, maxsum);
+        }
+    }
+    cout << maxsum;
+
+    return 0;
+}
